97a3b04c359f28d2b1c7ffd1014538b8ec26b4f7
LanzSalviejo <lanzsalviejo@gmail.com>
2024-08-26 16:18:29 -0700

    created line and point class

--- graphics.py
+++ graphics.py
@@ -4,10 +4,10 @@ class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("Maze Solver")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, bg="white", height=height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
         self.is_running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     
     def redraw(self):
@@ -19,7 +19,23 @@ class Window:
         while self.is_running:
             self.redraw()
         print("Window closed...")
+        
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+        
     
     def close(self):
         self.is_running = False
         
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import *
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(100,300), Point(200, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
