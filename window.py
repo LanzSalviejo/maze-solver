77aee8ab3bca97b9967279382d53bf8bae1bb57a
LanzSalviejo <lanzsalviejo@gmail.com>
2024-08-27 11:54:28 -0700

    Added comments

--- window.py
+++ window.py
@@ -2,12 +2,12 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.__root = Tk() #creates root widget
+        self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack()
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width) # our default convas
+        self.__canvas.pack() # packs canvas making it ready to be drawn
         self.__is_running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.protocol("WM_DELETE_WINDOW", self.close) # for when closing window
 
     
     def redraw(self):
