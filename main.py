1e3da1f1229f18d68e1a7bede88eee6f9e018040
LanzSalviejo <lanzsalviejo@gmail.com>
2024-08-27 12:19:13 -0700

    created point and line class

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from window import Window
+from window import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(100, 200), Point(300, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -4,15 +4,18 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width) # our default convas
-        self.__canvas.pack() # packs canvas making it ready to be drawn
-        self.__is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close) # for when closing window
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width) # our default convas
+        self.__canvas.pack(fill=BOTH, expand=1) # packs canvas making it ready to be drawn
+        self.__is_running = False
 
     
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
+        
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
     
     def wait_for_close(self):
         self._is_running = True
@@ -21,3 +24,16 @@ class Window:
     
     def close(self):
         self._is_running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
