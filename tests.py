d76b75b59983c04220385cff39610eea0947c7af
LanzSalviejo <lanzsalviejo@gmail.com>
2024-08-27 10:21:01 -0700

    created unittests

---
+++ tests.py
@@ -0,0 +1,36 @@
+import unittest
+from tkinter import Tk, Canvas
+from maze import Maze
+from graphics import Window
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win = None)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_large(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win = None)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
